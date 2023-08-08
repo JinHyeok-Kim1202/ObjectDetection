b68a6245e5009c5a3dbf178558b42ea44d3167e2
JinHyeok-Kim1202 <kristoper123457@gmail.com>
2023-08-08 13:53:21 +0900

    setInput, forward에 대한 정리

--- MainActivity.py
+++ MainActivity.py
@@ -33,9 +33,12 @@ while True:
     # ddepth        : 출력 블롭의 깊이. CV_32F 또는 CV_8U. default = CV_32F
     # retval        : 영상으로부터 구한 블롭 객체. numpy.ndarray.shpe=(N, C, H, W). N : 개수, C : 채널 개수, HW : 영상 크기
     blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    # setInput      : 입력 blob을 이용하여 네트워크 실행
+    # forward       : 네트워크 입력을 설정한 후 네트워크를 순방향으로 실행하여 결과를 예측하는 함수
     YOLO_net.setInput(blob)
     outs = YOLO_net.forward(output_layers)
 
+    # 이 뒤는 네트워크를 실행한 결과에 따라서 결과(사각테두리)를 출력하는 부분
     class_ids = []
     confidences = []
     boxes = []
